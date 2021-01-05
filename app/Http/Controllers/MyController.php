6eaece7000f9126924828a77dfd3e5d72139305e
HaSaN <mahamudul951@gmail.com>
2021-01-05 22:26:49 +0600

    login Signup

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function Login()
+    {
+        return view('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/llogin', [MyController::class, 'Login']);
